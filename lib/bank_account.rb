e1804624d6ca290858231861e8ea1a0005c4d039
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-21 13:13:43 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,20 @@
 class BankAccount
 
+
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = open
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    @balance
+  end
+
+  
 end
