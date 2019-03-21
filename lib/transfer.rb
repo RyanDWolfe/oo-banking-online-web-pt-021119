1979607e3b1672dfb6ca25178a515827c22e4146
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-21 13:26:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+
+  attr_accessor
+  attr_reader
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = amount
+  end
 end
